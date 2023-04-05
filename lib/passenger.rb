9d9f42f8fbb77e80c95ed121ad0af5fe584530db
Alex Kiselich <alexander.kiselich@gmail.com>
2023-04-05 09:33:32 -0600

    Add adult?, driver? and drive methods to passenger class and ran tests to see methods in action

--- lib/passenger.rb
+++ lib/passenger.rb
@@ -1,7 +1,24 @@
 class Passenger
-attr_reader :name, :age
+attr_reader :name, :age, :driver
   def initialize(info)
     @name = info["name"]
     @age = info["age"]
+    @driver = false
   end
+
+  def adult?
+    if self.age >= 18
+      true
+    else false
+    end
+  end
+
+  def driver?
+    @driver
+  end
+
+  def driver
+    @driver = true
+  end
+  
 end
--- spec/passenger_spec.rb
+++ spec/passenger_spec.rb
@@ -1,22 +1,41 @@
 require './lib/passenger'
+require 'pry'
 
 RSpec.describe Passenger do
   it 'exists' do
 
-  charlie = Passenger.new({"name" => "Charlie", "age" => 18})   
+    charlie = Passenger.new({"name" => "Charlie", "age" => 18})   
   
-  expect(charlie).to be_a Passenger
-  end
+    expect(charlie).to be_a Passenger
+    end
 
   it '#has attributes' do
 
-  charlie = Passenger.new({"name" => "Charlie", "age" => 18})
-  taylor = Passenger.new({"name" => "Taylor", "age" => 12}) 
+    charlie = Passenger.new({"name" => "Charlie", "age" => 18})
+    taylor = Passenger.new({"name" => "Taylor", "age" => 12}) 
 
-  expect(charlie.name).to eq("Charlie")
-  expect(taylor.age).to eq(12)
-  
+    expect(charlie.name).to eq("Charlie")
+    expect(taylor.age).to eq(12)
   end
 
+  it 'is an adult' do
+
+    charlie = Passenger.new({"name" => "Charlie", "age" => 18})
+    taylor = Passenger.new({"name" => "Taylor", "age" => 12}) 
+
+    expect(taylor.adult?).to eq(false)
+    expect(charlie.adult?).to eq(true)
+  end
+
+  it 'can become a driver' do
+    charlie = Passenger.new({"name" => "Charlie", "age" => 18})
+    taylor = Passenger.new({"name" => "Taylor", "age" => 12}) 
+
+    expect(charlie.driver?).to eq(false)
+
+    charlie.driver
+
+    expect(charlie.driver?).to eq(true)
+  end
 end
 
